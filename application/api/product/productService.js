adad0284375edc3e479d4c019d55281bbc620300
FabioCruz <fabinhocosta1202@gmail.com>
2018-06-05 18:42:12 -0300

    create handler messages

--- application/api/product/productService.js
+++ application/api/product/productService.js
@@ -1,6 +1,27 @@
 const Product = require('./product')
+const _ = require('lodash')
 
 Product.methods(['get','post','put','delete'])
 Product.updateOptions({ new:true, runValidators:true })
+
+Product.after('post', sendErrors).after('put', sendErrors)
+
+function sendErrors(req, res, next){
+    const bundle = res.locals.bundle
+
+    if(bundle.errors){
+        var errors = parseErrors(bundle.errors)
+        res.status(500).json({errors})
+    } else {
+        next()
+    }
+}
+
+function parseErrors(nodeErrors){
+    const errors = []
+    _.forIn(nodeErrors, error => errors.push(error.message))
+    return errors
+
+}
 
 module.exports = Product
